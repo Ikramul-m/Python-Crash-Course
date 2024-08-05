4f2271872e98aa485a16d9dc8c118e28a4a7a6e5
Ikramul-m <i20727172@gmail.com>
2024-08-05 22:57:10 +0530

    python

--- Start.py
+++ Start.py
@@ -2,9 +2,9 @@ message = "Hello Python World"
 print(message)
 
 message = "Hello Python crash course world"
-print(message.title())
-print(message.upper())
-print(message.lower())
+# print(message.title())
+# print(message.upper())
+# print(message.lower())
 
 first_name = "ada"
 last_name = "lovelace"
@@ -22,3 +22,7 @@ print(favourate_language.rstrip())
 
 favourate_language = ' Python '
 print(favourate_language.lstrip())
+
+message = "One of Python's strengths is its diverse community."
+# message = 'One of Python's strengths is its diverse community.'
+print(message)
