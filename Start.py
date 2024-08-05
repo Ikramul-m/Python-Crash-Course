b3186733fac5df6deb01e32b8fd8a90f31a364a5
Ikramul-m <i20727172@gmail.com>
2024-08-05 22:35:08 +0530

    new

--- Start.py
+++ Start.py
@@ -5,3 +5,8 @@ message = "Hello Python crash course world"
 # print(message.title())
 # print(message.upper())
 # print(message.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
